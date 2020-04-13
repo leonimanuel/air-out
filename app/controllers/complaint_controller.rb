777539efced8a5db29c17e2756dbae17358296aa
LIManuel <leonmalisov@gmail.com>
2020-04-13 10:51:25 -0400

    added edit/delete functionality for complaints

--- app/controllers/complaint_controller.rb
+++ app/controllers/complaint_controller.rb
@@ -1,26 +1,60 @@
+
 class ComplaintController < ApplicationController 
+  # before do
+  #   redirect '/' unless logged_in?
+  # end
 
 	get "/account/complaints" do
   	redirect "/login" if !Helpers.is_logged_in?(session)
 
-  	@user = User.find(Helpers.current_user(session))
+  	@user = Helpers.current_user(session)
 		erb :"complaints/index"
 	end	
 
 	get "/account/complaints/new" do
   	redirect "/login" if !Helpers.is_logged_in?(session)
-
   	@user = Helpers.current_user(session)
+		
 		erb :"complaints/new"
 	end
 
 	post "/account/complaints" do
 		content = params[:complaint][:content]
 		housemate = User.find(params[:complaint][:housemate])
-		user = User.find(Helpers.current_user(session))
-
+		user = Helpers.current_user(session)
 		complaint = Complaint.create(content: content, user: user, housemate: housemate)
-		# binding.pry
+
 		redirect "/account/complaints"
+	end
+
+	get "/account/charges" do
+  	redirect "/login" if !Helpers.is_logged_in?(session)
+  	@user = Helpers.current_user(session)
+
+		erb :"complaints/charges"
+	end
+
+	get "/account/complaints/:id/edit" do
+  	# binding.pry
+  	@complaint = Complaint.find(params[:id])
+  	redirect "/login" if !Helpers.is_logged_in?(session) ||
+  		@complaint.user != Helpers.current_user(session)
+  	# @user = Helpers.current_user(session)
+  	erb :"complaints/edit"
+	end
+
+  patch "/account/complaints/:id" do
+		complaint = Complaint.find(params[:id])
+		complaint.update(content: params[:content])
+		complaint.save    
+
+    redirect "/account/complaints"
+  end
+
+	delete "/account/complaints/:id/delete" do
+  	binding.pry
+  	complaint = Complaint.find(params[:id])
+
+  	redirect "/account/complaints"
 	end
 end
--- config.ru
+++ config.ru
@@ -1,9 +1,13 @@
 require './config/environment'
+require "./app/controllers/application_controller"
+
 
 if ActiveRecord::Migrator.needs_migration?
   raise 'Migrations are pending. Run `rake db:migrate` to resolve the issue.'
 end
 
+
+use Rack::MethodOverride
 use UserController
 use ComplaintController
 run ApplicationController
